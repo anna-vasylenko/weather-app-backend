4ec6272aad5e0025ad3e30f0ebe15f33aed7e5f5
anna-vasylenko <anna.bilych16@gmail.com>
2025-02-07 18:32:18 +0200

    init project

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { env } from './utils/env.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+
+  // app.use('/auth', authRouter);
+
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
